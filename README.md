93dc512616dcff43917aa687ac2e4e44596cb483
Ludovico Nocco <109106251+LudoNocco@users.noreply.github.com>
2024-08-23 13:20:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mooc-programming-24
+ Helsinki University Python courses I and II for 10 ECTS
---
+++ part01-08_name_and_address/src/name_and_address.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+surname = input("What is your surname? ")
+address = input("What is your street address? ")
+cityandpostcode = input("What is your city and post code? ")
+
+print(name, surname)
+print(address)
+print(cityandpostcode)
