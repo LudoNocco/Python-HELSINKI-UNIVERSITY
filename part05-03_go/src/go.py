3e0528a007793970589c2593263f92b731736dcf
Ludovico Nocco <109106251+LudoNocco@users.noreply.github.com>
2024-09-08 09:24:44 +0100

    Downloaded exercises for part 6

--- part05-03_go/src/go.py
+++ part05-03_go/src/go.py
@@ -1,1 +1,11 @@
-# Write your solution here
+def who_won(game_board: list) -> int:
+
+    player1_count = sum(row.count(1) for row in game_board)
+    player2_count = sum(row.count(2) for row in game_board)
+
+    if player1_count > player2_count:
+        return 1
+    elif player2_count > player1_count:
+        return 2
+    else:
+        return 0
